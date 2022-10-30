9348b77f6b319e69687ee1f4f97bc148df34938b
MelinaFE99 <erikamelinamamani@gmail.com>
2022-10-30 13:21:33 -0300

    Agregando responsive en recetas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { getRecipeRandom } from "../redux/reducers/recipes/recipes.actions";
-import Recipes from './Recipes';
+import Recipes from "./Recipes";
 
 const Home = () => {
-
-
   const recipeRandom = useSelector((state) => state.recipeReducer.recipeRandom);
   const query = useSelector((state) => state.recipeReducer.recipeName);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getRecipeRandom())
-  }, [dispatch]) 
-
+    dispatch(getRecipeRandom());
+  }, [dispatch]);
 
   return (
-    <div className='container'>
-    {
-      query.length > 1 ?
-      <Recipes query={query} />
-      :
-
-      <div className='card-random'>
-        { recipeRandom?.map( (recipe) => 
-          <div key={recipe.idMeal}>
-            <div className='recipeRandomHeader'>
-              <h2>{recipe.strMeal}</h2>
-              <Link className='linkDetail' to={"/detail/"+recipe.idMeal}>Mas info</Link>
+    <div className="container">
+      {query.length > 1 ? (
+        <Recipes query={query} />
+      ) : (
+        <div className="card-random">
+          {recipeRandom?.map((recipe) => (
+            <div key={recipe.idMeal} className="recipeRandomContainer">
+              <div className="recipeRandomHeader">
+                <h2>{recipe.strMeal}</h2>
+                <Link className="linkDetail" to={"/detail/" + recipe.idMeal}>
+                  Read more
+                </Link>
+              </div>
+              <img src={recipe.strMealThumb} />
             </div>
-            <img src={recipe.strMealThumb}/>
-          </div>
-        ) }
-      </div> 
-    }
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,36 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
 import { getTypes } from "../redux/reducers/recipes/recipes.actions";
 
 const Category = () => {
-    
-    const { categoryName } = useParams()
+  const { categoryName } = useParams();
 
+  const categories = useSelector((state) => state.recipeReducer.categories);
+  const dispatch = useDispatch();
 
-    const categories = useSelector((state) => state.recipeReducer.categories);
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getTypes(categoryName));
+  }, [categoryName, dispatch]);
 
-    
-    useEffect(() => {
-        dispatch(getTypes(categoryName))
-    }, [categoryName, dispatch])  
+  return (
+    <>
+      <div className="containerCategory">
+        {categories.map((recipe) => (
+          <div className="recipeCategory" key={recipe.idMeal}>
+            <img src={recipe.strMealThumb} />
+            <h3>{recipe.strMeal}</h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-
-    return (
-        <>
-
-        <div className='containerCategory'> 
-            {categories.map(recipe => 
-                <div className='recipeCategory' key={recipe.idMeal}>
-                    <img  src={recipe.strMealThumb}/>
-                    <h2>{recipe.strMeal}</h2>
-                </div>
-            )}
-        </div>
-
-        </>
-  )
-}
-
-export default Category
+export default Category;
