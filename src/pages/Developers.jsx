1666e07252fe8a95323dc97271cc128ed98342d4
Daiana Gimenez <namined09@gmail.com>
2022-12-05 14:32:50 -0300

    Se arreglo el input Search

---
+++ src/pages/Developers.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Developers = () => {
+  return (
+    <div >
+      <h1>Developers</h1>
+
+    <div className="developers">
+      <div>
+        <h2>Melina Mamaní</h2>
+        <span>Frontend Developer</span>
+        <ul>
+          <li>LinkedIn</li>
+          <li>GitHub</li>
+          <li>Email</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Daiana Elizabeth Gimenez</h2>
+        <span>Frontend Developer</span>
+          <ul>
+            <li>LinkedIn</li>
+            <li>GitHub</li>
+            <li>Email</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Developers
